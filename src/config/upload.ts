a352437b7d0912fd5a7f9016366b463f7eab2f6a
daniel21h <hesseldaniel21@gmail.com>
2020-04-24 11:24:49 -0300

    upload files

---
+++ src/config/upload.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(request, file, callback) {
+      // Certificando de que sempre o arquivo será salvo com um nome único
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
